22c044c6c1b68142a90b85aec5361dce33aa0111
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-05 14:20:10 -0700

    dot env

--- apolloClient.js
+++ apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api-ca-central-1.hygraph.com/v2/clg1dpl1r89y501ue0ava00ft/master",
+  uri: process.env.HYGRAPH_URL,
   cache: new InMemoryCache(),
 });
 
