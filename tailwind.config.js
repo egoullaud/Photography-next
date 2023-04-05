bdd05a1c8cd935709a9bef488dacc557606d40f4
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-05 14:33:57 -0700

    baskerville-libre added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        baskerville: ["Baskerville", "serif"],
+      },
+    },
   },
   plugins: [],
 };
