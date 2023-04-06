816ea9e374562efb113862335f88bc2112d800c2
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-05 17:21:28 -0700

    hooked up

---
+++ pages/galleries/videography.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function videography() {
+  return (
+    <div>
+      {" "}
+      <h1 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
+        videography gallery
+      </h1>
+    </div>
+  );
+}
+
+export default videography;
