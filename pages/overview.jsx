1e5f2132a41bcaeaeceb35a52377356f992cea81
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-06 16:09:07 -0700

    imports changed

--- pages/overview.jsx
+++ pages/overview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Galleries from "@/components/Galleries";
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Galleries from "../components/Galleries";
 
 export default function Overview() {
   return (
