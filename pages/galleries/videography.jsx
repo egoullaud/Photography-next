90b924e06be4903ba1fcc51b62549d7aac8fc6f1
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-07 15:09:39 -0700

    done

--- pages/galleries/videography.jsx
+++ pages/galleries/videography.jsx
@@ -9,24 +9,28 @@ function videography({ videos }) {
       <h1 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
         videography gallery
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-[2rem] ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:m-[2rem] m-4 ">
         {videos.map((video) => (
-          <div className="flex flex-col justify-start items-center text-center">
+          <div
+            key={video.id}
+            className="flex flex-col justify-start items-center text-center "
+          >
             <iframe
+              className=" w-[100%]"
               width="560"
               height="315"
               src={video.embedUrl}
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
 
             <h1 className="pt-4 mx-4 text-xl uppercase font-semibold tracking-wider">
               {video.title}
             </h1>
             <p
-              className="mx-8 my-2 tracking-wide"
+              className="mx-2 lg:mx-8 my-2 tracking-wide"
               dangerouslySetInnerHTML={{ __html: video.description.html }}
             ></p>
           </div>
