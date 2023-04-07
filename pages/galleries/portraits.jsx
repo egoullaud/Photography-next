0a7e4d49fea7faaa4549ba3d5781f199dc537190
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-07 12:36:30 -0700

    modal formatted

--- pages/galleries/portraits.jsx
+++ pages/galleries/portraits.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { gql } from "@apollo/client";
 import client from "../../apolloClient";
 import Image from "next/image";
+import { BsChevronDoubleRight } from "react-icons/bs";
 
 function Portraits({ images }) {
   const [showModal, setShowModal] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const modalRef = useRef();
 
   const handleImageClick = (index) => {
     setCurrentIndex(index);
@@ -28,6 +30,20 @@ function Portraits({ images }) {
     }
   };
 
+  useEffect(() => {
+    const handleClickOutsideModal = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShowModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutsideModal);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideModal);
+    };
+  }, []);
+
   return (
     <div>
       <h1 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
@@ -48,9 +64,9 @@ function Portraits({ images }) {
       </div>
       {showModal && (
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 flex justify-center items-center">
-          <div className="relative w-5/6 h-5/6">
+          <div className="relative w-3/4 h-5/6" ref={modalRef}>
             <button
-              className="absolute top-0 right-0 text-white p-2"
+              className="absolute z-10 top-0 right-0 rounded-full px-3 py-1 text-xl uppercase bg-white text-black hover:bg-[#161616] hover:text-white hover:transition-all hover:duration-500"
               onClick={handleModalClose}
             >
               X
@@ -64,17 +80,17 @@ function Portraits({ images }) {
                 className="max-w-full max-h-full object-contain"
               />
             </div>
-            <div className="absolute top-1/2 left-0 transform-translate-y-1/2">
+            <div className="absolute top-1/2 left-5 transform-translate-y-1/2">
               <button
-                className="bg-white text-black p-2 rounded-full mr-2"
+                className="bg-white text-black  px-4 py-2 rounded-full mr-2 uppercase  hover:bg-[#161616] hover:text-white hover:transition-all hover:duration-500"
                 onClick={() => handleArrowClick("prev")}
               >
                 Prev
               </button>
             </div>
-            <div className="absolute top-1/2 right-0 transform-translate-y-1/2">
+            <div className="absolute top-1/2 right-5 transform-translate-y-1/2">
               <button
-                className="bg-white text-black p-2 rounded-full"
+                className="bg-white text-black px-4 py-2 rounded-full uppercase  hover:bg-[#161616] hover:text-white hover:transition-all hover:duration-500"
                 onClick={() => handleArrowClick("next")}
               >
                 Next
