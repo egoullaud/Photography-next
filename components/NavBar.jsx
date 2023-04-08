067a987b046c4c7365e9af747748f8b96b8be3e3
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-07 20:12:07 -0700

    back to props

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 function NavBar({ categories }) {
+  console.log(categories);
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -15,25 +16,27 @@ function NavBar({ categories }) {
                       md:my-[3rem]
                         lg:my-[4rem]"
         >
-          <Link href="/">
-            <h1 className="text-center text-3xl md:text-5xl lg:text-7xl leading-none tracking-widest">
-              Raine Gauthier
-            </h1>
-            <h2 className=" text-xl md:text-3xl lg:text-4xl tracking-widest">
-              Photography
-            </h2>
-          </Link>
+          <h1 className="text-center text-3xl md:text-5xl lg:text-7xl leading-none tracking-widest">
+            Raine Gauthier
+          </h1>
+          <h2 className=" text-xl md:text-3xl lg:text-4xl tracking-widest">
+            Photography
+          </h2>
         </div>
-        <ul className=" hidden lg:flex justify-center w-[100%] lg:text-xl">
-          {categories?.map((category) => (
-            <li
-              className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-1000 p-2 px-4 uppercase"
-              key={category.id}
-            >
-              <Link href={`/galleries/${category.slug}`}>{category.title}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className=" hidden lg:flex justify-center lg:text-xl">
+          <ul className=" hidden lg:flex justify-center lg:text-xl">
+            {categories &&
+              categories?.map((category) => (
+                <li
+                  className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-1000 p-2 px-4 uppercase"
+                  key={category.id}
+                >
+                  <Link href={`/${category.slug}`}>{category.title}</Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+
         {/* Mobile navigation */}
         <button
           className="block lg:hidden w-[12rem] text-center hover:bg-[#363636] text-xl tracking-wide hover:text-white hover:transition-all hover:duration-1000 p-2 px-4 uppercase mx-auto z-10"
@@ -43,18 +46,18 @@ function NavBar({ categories }) {
           Menu
         </button>
         {showMenu && (
-          <ul className=" lg:hidden flex flex-col justify-center items-center w-[100%] lg:text-xl">
-            {categories?.map((category) => (
-              <li
-                className="w-[12rem] text-center hover:bg-[#363636] text-xl tracking-wide hover:text-white hover:transition-all hover:duration-1000 p-2 px-4 uppercase"
-                key={category.id}
-              >
-                <Link href={`/galleries/${category.slug}`}>
-                  {category.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <div className=" lg:hidden flex flex-col justify-center items-center w-[100%] lg:text-xl">
+            <ul>
+              {categories?.map((category) => (
+                <li
+                  className="w-[12rem] text-center hover:bg-[#363636] text-xl tracking-wide hover:text-white hover:transition-all hover:duration-1000 p-2 px-4 uppercase"
+                  key={category.id}
+                >
+                  <Link href={`/${category.slug}`}>{category.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </nav>
     </div>
