13f750d2c124c9be033d3ee66457cb1358b86b87
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-07 20:12:17 -0700

    back to getinitialprops

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
+import client from "../apolloClient";
+import { gql } from "@apollo/client";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, categoriesData }) {
   return (
     <>
-      <NavBar />
+      <NavBar categories={categoriesData?.categories} />
       <Component {...pageProps} />
       <Footer />
     </>
   );
 }
+MyApp.getInitialProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query Categories {
+        categories(orderBy: listOrder_ASC) {
+          title
+          id
+          slug
+        }
+      }
+    `,
+  });
+
+  return { categoriesData: data };
+};
