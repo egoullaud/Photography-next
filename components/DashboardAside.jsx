d0b601145272ebd821f9513b2637bd21ca3a753a
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 19:23:48 -0700

    added link to site

--- components/DashboardAside.jsx
+++ components/DashboardAside.jsx
@@ -1,39 +1,41 @@
 import React from "react";
+import Link from "next/link";
 
-function DashboardAside() {
+function DashboardAside({ categories }) {
   return (
     <div className="fixed top-0 bottom-0 left-0 bg-[#363636] text-white w-[20%] ">
       <div className="py-4">
         <h1 className="text-4xl p-2">Welcome, Raine</h1>
       </div>
       <ul className="flex flex-col">
-        <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Upload Photos
-        </li>
+        <Link href="/admin/upload">
+          {" "}
+          <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+            Upload Photos
+          </li>
+        </Link>
         <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
           Edit Galleries{" "}
         </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Architecture
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Events
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Portraits
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Nature
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Light
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Videography
-        </li>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Back to Website
-        </li>
+        {categories &&
+          categories?.map((category) => (
+            <Link href={`/admin/edit/${category.slug}`}>
+              {" "}
+              <li
+                className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500"
+                key={category.id}
+              >
+                {category.title}
+              </li>
+            </Link>
+          ))}
+
+        <Link href="/">
+          {" "}
+          <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+            Back to Website
+          </li>
+        </Link>
         <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
           Log Out
         </li>
