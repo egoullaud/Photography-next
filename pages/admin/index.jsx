73e6a0769fb5e839bfa47f462cfa7c985e72cc3c
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 17:24:16 -0700

    dashboard

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -2,10 +2,23 @@ import React from "react";
 
 function dashboard() {
   return (
-    <div>
-      <h1 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
-        Welcome, Raine!
-      </h1>
+    <div className="flex h-[100vh]">
+      <div className=" flex flex-col w-[20%] border-r-2">
+        <h1 className="uppercase text-2xl text-center my-[1rem] tracking-wider">
+          Welcome, Raine!
+        </h1>
+        <button>Upload Photos</button>
+        <ul className="text-center">
+          Edit Galleries
+          <li>Architecture</li>
+          <li>Events</li>
+          <li>Portraits</li>
+          <li>Nature</li>
+          <li>Light</li>
+          <li>Videography</li>
+        </ul>
+      </div>
+      <div></div>
     </div>
   );
 }
