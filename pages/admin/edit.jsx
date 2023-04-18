284e53f16339261aaffa5cf6e8ccd97a37f55714
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 17:24:30 -0700

    created with mutations

---
+++ pages/admin/edit.jsx
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+import React, { useState } from "react";
+
+function edit() {}
+return (
+  <div className="flex">
+    <div className="w-[20%]">Aside</div>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
+        Edit Galleries
+      </h2>
+    </div>
+  </div>
+);
+
+export default edit;
+
+// Mutations
+
+//Edit Photo - select photo
+export const SELECT_PHOTO = gql`
+  mutation selectPhoto($slug: String!) {
+    updateImage(data: { selected: "true" }, where: { slug: $slug })
+  }
+`;
+
+//Add Photo
+export const CREATE_IMAGE_MUTATION = gql`
+  mutation createImage {
+    createImage(
+      data: {
+        title: ""
+        slug: ""
+        image: { create: { fileName: "", height: 1.5, width: 1.5 } }
+        category: { connect: { slug: "" } }
+      }
+    ) {
+      id
+      category {
+        slug
+        title
+      }
+      slug
+      title
+      image {
+        height
+        id
+        url
+        width
+      }
+    }
+  }
+`;
+
+// Delete Selected Photos
+export const DELETE_SELECTED_MUTATION = gql`
+  mutation deleteSelected {
+    deleteImage(where: { selected: true })
+  }
+`;
+
+//publish changed photo
+export const PUBLISH_IMAGE = gql`
+  mutation publishImage {
+    publishImage(where: { slug: "" }, to: PUBLISHED) {
+      slug
+    }
+  }
+`;
