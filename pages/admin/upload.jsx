b5f0c311d3736125bee6c04b5e7238f4c0ef1cd8
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 18:46:59 -0700

    dashbaord menu

--- pages/admin/upload.jsx
+++ pages/admin/upload.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import DashboardLayout from "../../components/DashboardLayout";
 
 function Upload() {
   const [title, setTitle] = useState("");
@@ -28,71 +29,73 @@ function Upload() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h2 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
-        Add New Photo
-      </h2>
-      <p className="mb-[2rem]">Maximum size: 100mb</p>
-      <form
-        method="post"
-        onSubmit={handleOnSubmit}
-        encType="multipart/form-data"
-        className="flex flex-col justify-around items-center mb-[4rem] lg:w-[90%]"
-      >
-        <div className="flex justify-around items-center mb-[4rem] lg:w-[90%]">
-          <div className="w-[30%] mx-2">
-            <input
-              required
-              type="text"
-              placeholder="Title"
-              className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
-              name="title"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            />
-          </div>
-          <div className="w-[30%]">
-            <select
-              required
-              id="category"
-              name="category"
-              className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
-              value={category}
-              onChange={(event) => setCategory(event.target.value)}
-            >
-              <option value="">Select Category</option>
-              <option value="architecture">Architecture</option>
-              <option value="Events">Events</option>
-              <option value="Portraits">Portraits</option>
-              <option value="Nature">Nature</option>
-              <option value="Light">Light</option>
-              <option value="Videography">Videography</option>
-            </select>
-          </div>
-          <div className="w-[30%]">
-            <input
-              required
-              type="file"
-              name="file"
-              className="p-3 mb-2 w-[100%] tracking-wide"
-              onChange={handleOnChange}
-            />
-          </div>
-
-          <div className="w-[30%]">
-            {imageSrc && (
-              <button
-                className="border-2 border-[#363636] p-2 shadow-lg my-2 w-[60%] lg:w-[45%] hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-1000 tracking-wide"
-                type="submit"
+    <DashboardLayout>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
+          Add New Photo
+        </h2>
+        <p className="mb-[2rem]">Maximum size: 100mb</p>
+        <form
+          method="post"
+          onSubmit={handleOnSubmit}
+          encType="multipart/form-data"
+          className="flex flex-col justify-around items-center mb-[4rem] lg:w-[90%]"
+        >
+          <div className="flex justify-around items-center mb-[4rem] lg:w-[90%]">
+            <div className="w-[30%] mx-2">
+              <input
+                required
+                type="text"
+                placeholder="Title"
+                className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
+                name="title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </div>
+            <div className="w-[30%]">
+              <select
+                required
+                id="category"
+                name="category"
+                className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
+                value={category}
+                onChange={(event) => setCategory(event.target.value)}
               >
-                Upload Photo
-              </button>
-            )}
+                <option value="">Select Category</option>
+                <option value="architecture">Architecture</option>
+                <option value="Events">Events</option>
+                <option value="Portraits">Portraits</option>
+                <option value="Nature">Nature</option>
+                <option value="Light">Light</option>
+                <option value="Videography">Videography</option>
+              </select>
+            </div>
+            <div className="w-[30%]">
+              <input
+                required
+                type="file"
+                name="file"
+                className="p-3 mb-2 w-[100%] tracking-wide"
+                onChange={handleOnChange}
+              />
+            </div>
+
+            <div className="w-[30%]">
+              {imageSrc && (
+                <button
+                  className="border-2 border-[#363636] p-2 shadow-lg my-2 w-[60%] lg:w-[45%] hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-1000 tracking-wide"
+                  type="submit"
+                >
+                  Upload Photo
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-        {imageSrc && <img src={imageSrc} />}
-      </form>
-    </div>
+          {imageSrc && <img src={imageSrc} />}
+        </form>
+      </div>
+    </DashboardLayout>
   );
 }
 
