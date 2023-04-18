7851bf54cffc0824c87c36058adedcd020a5c093
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 18:46:19 -0700

    dash footer

--- components/DashboardFooter.jsx
+++ components/DashboardFooter.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function DashboardFooter() {
-  return <div>Footer</div>;
+  return (
+    <div className="fixed bottom-0 right-0 left-0 text-center bg-[#363636] text-white font-bold">
+      <h1 className="text-2xl p-2">Raine Gauthier Photography</h1>
+      <h2 className=" px-2 pb-1"> Created by Lee Goullaud 2023</h2>
+    </div>
+  );
 }
 
 export default DashboardFooter;
