b15d03d3f7b64270894572534f5c55d449ed58cc
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 17:16:11 -0700

    admin dashbaord page

---
+++ pages/admin/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function dashboard() {
+  return (
+    <div>
+      <h1 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
+        Welcome, Raine!
+      </h1>
+    </div>
+  );
+}
+
+export default dashboard;
