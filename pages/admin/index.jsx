e96dadbeca974e15ea44d23a5452aa5ef1b601c8
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-17 18:46:49 -0700

    created aside menu

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,25 +1,16 @@
 import React from "react";
+import DashboardLayout from "../../components/DashboardLayout";
+import DashboardAside from "../../components/DashboardAside";
 
 function dashboard() {
   return (
-    <div className="flex h-[100vh]">
-      <div className=" flex flex-col w-[20%] border-r-2">
-        <h1 className="uppercase text-2xl text-center my-[1rem] tracking-wider">
-          Welcome, Raine!
-        </h1>
-        <button>Upload Photos</button>
-        <ul className="text-center">
-          Edit Galleries
-          <li>Architecture</li>
-          <li>Events</li>
-          <li>Portraits</li>
-          <li>Nature</li>
-          <li>Light</li>
-          <li>Videography</li>
-        </ul>
+    <DashboardLayout>
+      <div className="flex">
+        <DashboardAside />
+
+        <div></div>
       </div>
-      <div></div>
-    </div>
+    </DashboardLayout>
   );
 }
 
