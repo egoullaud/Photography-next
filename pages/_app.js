8a82cc92fbdc3bce7e7296e7b3f5094b1bd05868
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-19 17:25:58 -0700

    apolloprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+
+import client from "../apolloClient";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
   );
 }
