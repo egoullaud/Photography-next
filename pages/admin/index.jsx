08a271ee2d6033830a016c648063db74e88cfbd5
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-19 17:26:37 -0700

    emptied

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -2,100 +2,55 @@ import React, { useState } from "react";
 import DashboardLayout from "../../components/DashboardLayout";
 import client from "../../apolloClient";
 import { gql } from "@apollo/client";
+import Link from "next/link";
 
 function Upload({ categories }) {
-  const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const [file, setFile] = useState(null);
-  const [imageSrc, setImageSrc] = useState(null);
-
-  const handleOnChange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (onLoadEvent) {
-      setImageSrc(onLoadEvent.target.result);
-    };
-
-    reader.readAsDataURL(file);
-    setFile(file);
-  };
-
-  const handleOnSubmit = async (event) => {
-    event.preventDefault();
-    console.log("Title:", title);
-    console.log("Category:", category);
-    console.log("File:", file);
-
-    // You can add your mutation here to create the image
-  };
-
   return (
     <DashboardLayout categories={categories}>
-      <div className="flex flex-col w-[100%] items-center justify-end">
-        <h2 className="uppercase text-3xl text-center mt-[2rem] tracking-wider">
-          Add New Photo
-        </h2>
-        <p className="mb-[2rem]">Maximum size: 100mb</p>
-        <form
-          method="post"
-          onSubmit={handleOnSubmit}
-          encType="multipart/form-data"
-          className="flex flex-col justify-around items-center mb-[4rem] lg:w-[90%]"
-        >
-          <div className="flex justify-around items-center mb-[4rem] lg:w-[90%]">
-            <div className="w-[30%] mx-2">
-              <input
-                required
-                type="text"
-                placeholder="Title"
-                className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
-                name="title"
-                value={title}
-                onChange={(event) => setTitle(event.target.value)}
-              />
-            </div>
-            <div className="w-[30%]">
-              <select
-                required
-                id="category"
-                name="category"
-                className="border-2 border-[#363636] p-2 shadow-lg mb-2 w-[100%] tracking-wide"
-                value={category}
-                onChange={(event) => setCategory(event.target.value)}
-              >
-                <option value="">Select Category</option>
-                <option value="architecture">Architecture</option>
-                <option value="Events">Events</option>
-                <option value="Portraits">Portraits</option>
-                <option value="Nature">Nature</option>
-                <option value="Light">Light</option>
-                <option value="Videography">Videography</option>
-              </select>
-            </div>
-            <div className="w-[30%]">
-              <input
-                required
-                type="file"
-                name="file"
-                className="p-3 mb-2 w-[100%] tracking-wide"
-                onChange={handleOnChange}
-              />
-            </div>
+      <div className="flex flex-col justify-center items-end">
+        <div className="w-[100%]">
+          <div className="flex flex-col items-center">
+            <h2 className="uppercase text-3xl text-center mt-[4rem] mb-[2rem] tracking-wider">
+              Add Photo on Hygraph
+            </h2>
 
-            <div className="w-[30%]">
-              {imageSrc && (
-                <button
-                  className="border-2 border-[#363636] p-2 shadow-lg my-2 w-[60%] lg:w-[45%] hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-1000 tracking-wide"
-                  type="submit"
-                >
-                  Upload Photo
-                </button>
-              )}
-            </div>
+            <Link href="https://hygraph.com/" target="_blank">
+              <button className="mb-[2rem] text-lg p-3 border-[1px] border-[#363636] hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 ">
+                Go to Hygraph
+              </button>
+            </Link>
           </div>
-          {imageSrc && <img src={imageSrc} />}
-        </form>
+
+          <div className="flex justify-around items-center  ">
+            <ol className="list-decimal">
+              {" "}
+              <span className="text-xl">Add a Photo</span>
+              <li>Log in to Hygraph</li>
+              <li>Click "Content" on left-hand side</li>
+              <li>Click "Add Entry" on top right</li>
+              <li>
+                Click add Image, and select from your desktop (Max size 100mb)
+              </li>
+              <li>Add title of photo</li>
+              <li>
+                Add category: Click on "Add existing category" and select
+                category
+              </li>
+            </ol>
+            <ol className="list-decimal">
+              <span className="text-xl">Add a Video</span>
+              <li>Select "Video" from content list </li>
+              <li>Click "Add Entry" on top right</li>
+              <li>Click add Video and click the "Upload" button</li>
+              <li>Copy/paste the Youtube URL to upload the video.</li>
+              <li>Add in the description and title</li>
+              <li>
+                Add to Videography: select "add existing category" and select
+                videography
+              </li>
+            </ol>
+          </div>
+        </div>
       </div>
     </DashboardLayout>
   );
