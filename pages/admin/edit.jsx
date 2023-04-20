128546eb1295a77f4be724a628897f7f3565271b
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-20 10:41:14 -0700

    empty out for cloudinary

--- pages/admin/edit.jsx
+++ pages/admin/edit.jsx
@@ -1,66 +1,7 @@
-import { gql } from "@apollo/client";
-import React, { useState } from "react";
-import DashboardLayout from "../../components/DashboardLayout";
+import React from "react";
 
 function edit() {
-  return (
-    <DashboardLayout>
-      <div className="flex"></div>
-    </DashboardLayout>
-  );
+  return <div></div>;
 }
 
 export default edit;
-
-// Mutations
-
-//Edit Photo - select photo
-export const SELECT_PHOTO = gql`
-  mutation selectPhoto($slug: String!) {
-    updateImage(data: { selected: "true" }, where: { slug: $slug })
-  }
-`;
-
-//Add Photo
-export const CREATE_IMAGE_MUTATION = gql`
-  mutation createImage {
-    createImage(
-      data: {
-        title: ""
-        slug: ""
-        image: { create: { fileName: "", height: 1.5, width: 1.5 } }
-        category: { connect: { slug: "" } }
-      }
-    ) {
-      id
-      category {
-        slug
-        title
-      }
-      slug
-      title
-      image {
-        height
-        id
-        url
-        width
-      }
-    }
-  }
-`;
-
-// Delete Selected Photos
-export const DELETE_SELECTED_MUTATION = gql`
-  mutation deleteSelected {
-    deleteImage(where: { selected: true })
-  }
-`;
-
-//publish changed photo
-export const PUBLISH_IMAGE = gql`
-  mutation publishImage {
-    publishImage(where: { slug: "" }, to: PUBLISHED) {
-      slug
-    }
-  }
-`;
