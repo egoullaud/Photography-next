55877a4ee65e7177900a02911b1a37884278b45a
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-21 16:22:08 -0700

    removed apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import "../styles/globals.css";
-import { ApolloProvider } from "@apollo/client";
-
-import client from "../apolloClient";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
