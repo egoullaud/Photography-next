c6b56d5b770b424f1dea6706e264662c32e39851
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-21 15:57:08 -0700

    edited home page

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,10 @@ import portraits from "../public/portrait-hero.jpg";
 import videography from "../public/music-video.jpg";
 import heroImg from "../public/hero-img.jpg";
 import Layout from "../components/Layout";
-import client from "../apolloClient";
-import { CATEGORIES_QUERY } from "../services/queries.js";
 
-function home({ categories }) {
+function home() {
   return (
-    <Layout categories={categories}>
+    <Layout>
       <div>
         <Head>
           <title>Raine Gauthier Photography</title>
@@ -27,8 +25,8 @@ function home({ categories }) {
         </Head>
 
         <main>
+          {/* hero area */}
           <div className="flex flex-col items-center justify-center  lg:mt-[5rem]">
-            {/* Hero area */}
             <div
               className="flex justify-center items-center text-center m-4
                         md:text-xl md:mx-[2rem]
@@ -43,6 +41,8 @@ function home({ categories }) {
             </div>
             <Image priority src={heroImg} className="" alt="/" />
           </div>
+
+          {/* services area */}
           <div className="flex flex-col ">
             <h1
               className="uppercase text-3xl text-center pt-[2rem] border-t-2 border-[#363636] tracking-wider
@@ -113,94 +113,9 @@ function home({ categories }) {
             </div>
             {/* <Cta /> */}
           </div>
-          <div className="px-2 lg:mt-[5rem] ">
-            <h3 className="uppercase text-3xl text-center pt-[2rem] tracking-wider border-t-2 border-[#363636] lg:text-5xl">
-              Galleries
-            </h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-2 px-2 py-4 pb-[5rem]">
-              <Link href="/architecture">
-                <div className="shadow-2xl ">
-                  <h4 className="uppercase text-xl text-center">
-                    Architecture
-                  </h4>
-                  <Image
-                    src={arch}
-                    alt="#"
-                    className="h-52 object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-              <Link href="/events">
-                <div className="shadow-2xl">
-                  <h4 className="uppercase text-xl text-center">events</h4>
-                  <Image
-                    src={events}
-                    alt="#"
-                    className="h-52 object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-              <Link href="/nature">
-                <div className="shadow-2xl">
-                  <h4 className="uppercase text-xl text-center">nature</h4>
-                  <Image
-                    src={nature}
-                    alt="#"
-                    className="h-52 object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-              <Link href="/light">
-                <div className="shadow-2xl">
-                  <h4 className="uppercase text-xl text-center">light</h4>
-                  <Image
-                    src={light}
-                    alt="#"
-                    className="h-52 object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-              <Link href="/portraits">
-                <div className="shadow-2xl">
-                  <h4 className="uppercase text-xl text-center hover:scale-105 hover:transition-all hover:duration-1000">
-                    portraits
-                  </h4>
-                  <Image
-                    src={portraits}
-                    alt="#"
-                    className="h-52 object-top object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-              <Link href="/videography">
-                <div className="shadow-2xl">
-                  <h4 className="uppercase text-xl text-center">videography</h4>
-                  <Image
-                    src={videography}
-                    alt="#"
-                    className="h-52 object-cover hover:scale-105 hover:transition-all hover:duration-1000"
-                  />
-                </div>
-              </Link>
-            </div>
-          </div>
         </main>
       </div>
     </Layout>
   );
 }
 export default home;
-
-export async function getStaticProps() {
-  const { data: categoriesData } = await client.query({
-    query: CATEGORIES_QUERY,
-  });
-  const categories = categoriesData.categories;
-
-  return {
-    props: {
-      categories,
-    },
-    revalidate: 86400,
-  };
-}
