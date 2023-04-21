dd5e9de639846e89bd36b850f8341f51f065eb04
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-21 15:56:48 -0700

    added cloudinary paths

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
-function NavBar({ categories }) {
-  console.log(categories);
+function NavBar() {
+  console.log();
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <div className="mx-2 text-[#363636] mb-[1rem] border-b-2 border-[#363636]">
       <nav>
@@ -16,30 +17,29 @@ function NavBar({ categories }) {
                       md:my-[3rem]
                         lg:my-[4rem]"
         >
-          <h1 className="text-center text-3xl md:text-5xl lg:text-7xl leading-none tracking-widest">
-            Raine Gauthier
-          </h1>
-          <h2 className=" text-xl md:text-3xl lg:text-4xl tracking-widest">
-            Photography
-          </h2>
+          <Link href="/">
+            <h1 className="text-center text-3xl md:text-5xl lg:text-7xl leading-none tracking-widest">
+              Raine Gauthier
+            </h1>
+            <h2 className=" text-xl md:text-3xl lg:text-4xl tracking-widest">
+              Photography
+            </h2>
+          </Link>
         </div>
         <div className=" hidden lg:flex justify-center lg:text-xl">
           <ul className=" hidden lg:flex justify-center lg:text-xl">
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
-              <Link href="/overview">overview</Link>
+              <Link href="/">overview</Link>
             </li>
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
               <Link href="/about">about</Link>
             </li>
-            {categories &&
-              categories?.map((category) => (
-                <li
-                  className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase"
-                  key={category.id}
-                >
-                  <Link href={`/${category.slug}`}>{category.title}</Link>
-                </li>
-              ))}
+            <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
+              <Link href="/galleries">galleries</Link>
+            </li>
+            <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
+              <Link href="/videography">videography</Link>
+            </li>
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
               <Link href="/contact">contact</Link>
             </li>
@@ -57,20 +57,14 @@ function NavBar({ categories }) {
         {showMenu && (
           <ul className=" lg:hidden flex flex-col justify-center items-center w-[100%] lg:text-xl">
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase w-[12rem] text-center">
-              <Link href="/overview">overview</Link>
+              <Link href="/">overview</Link>
             </li>
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase  w-[12rem] text-center">
               <Link href="/about">about</Link>
             </li>
-            {categories &&
-              categories?.map((category) => (
-                <li
-                  className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase  w-[12rem] text-center"
-                  key={category.id}
-                >
-                  <Link href={`/${category.slug}`}>{category.title}</Link>
-                </li>
-              ))}
+            <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase">
+              <Link href="/galleries">galleries</Link>
+            </li>
             <li className="hover:bg-[#363636] hover:text-white hover:transition-all hover:duration-500 ease-out duration-500 p-2 px-4 uppercase  w-[12rem] text-center">
               <Link href="/contact">contact</Link>
             </li>
