e437e36f3468dd3e0d4f6c88c629bed06c7af983
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-21 18:09:34 -0700

    updated

--- components/DashboardAside.jsx
+++ components/DashboardAside.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import Link from "next/link";
 
-function DashboardAside({ categories }) {
+function DashboardAside() {
   return (
     <div className="fixed top-0 bottom-0 left-0 bg-[#363636] text-white w-[20%] ">
       <div className="py-4">
         <h1 className="text-4xl p-2">Welcome, Raine</h1>
       </div>
       <ul className="flex flex-col">
-        <Link href="/admin/">
+        <Link href="/admin/edit">
           {" "}
           <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
             Upload Photos
           </li>
         </Link>
-        <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Edit Galleries{" "}
-        </li>
-        {categories &&
-          categories?.map((category) => (
-            <Link key={category.id} href={`/admin/edit/${category.slug}`}>
-              {" "}
-              <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-                {category.title}
-              </li>
-            </Link>
-          ))}
+        <Link href="/admin">
+          <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+            Edit Galleries{" "}
+          </li>
+        </Link>
 
         <Link href="/">
           {" "}
