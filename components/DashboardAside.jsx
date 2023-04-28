7094e7fde2742901e825cc108a1b3d23dd56039d
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-28 16:49:02 -0700

    toggle dash with icons

--- components/DashboardAside.jsx
+++ components/DashboardAside.jsx
@@ -1,30 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { TiArrowBack } from "react-icons/ti";
+import {
+  BiLogOutCircle,
+  BiChevronsRight,
+  BiChevronsLeft,
+} from "react-icons/bi";
+import { FaRegEdit } from "react-icons/fa";
 
 function DashboardAside() {
-  return (
-    <div className="fixed top-0 bottom-0 left-0 bg-[#363636] text-white w-[20%] ">
-      <div className="py-4">
-        <h1 className="text-4xl p-4">Welcome, Raine</h1>
-      </div>
-      <ul className="flex flex-col">
-        <Link href="/admin/edit">
-          {" "}
-          <li className="p-4  text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-            Upload & Edit Photos
-          </li>
-        </Link>
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-        <Link href="/">
-          {" "}
-          <li className="p-4 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-            Back to Website
+  const toggleSideBar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <div className="fixed top-0 bottom-0 left-0 bg-[#363636] text-white  ">
+      <button onClick={toggleSideBar} className="flex w-[100%]">
+        {!isCollapsed ? (
+          <h1 className=" flex text-4xl p-4">
+            Welcome, Raine{" "}
+            <BiChevronsLeft className="text-4xl mt-1 ml-1 hover:rounded-full hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500" />
+          </h1>
+        ) : (
+          <BiChevronsRight className="text-5xl w-full text-center  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500" />
+        )}
+      </button>
+      {isCollapsed && (
+        <ul className="flex flex-col items-center justify-center w-[100%]">
+          <Link href="/admin/edit">
+            <li className="w-full p-4 text-3xl  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+              {" "}
+              <FaRegEdit />
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="w-full p-4 text-3xl  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+              {" "}
+              <TiArrowBack />
+            </li>
+          </Link>
+
+          <li className="w-full p-4 text-3xl  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+            {" "}
+            <BiLogOutCircle />
           </li>
-        </Link>
-        <li className="p-4 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-          Log Out
-        </li>
-      </ul>
+        </ul>
+      )}
+      {!isCollapsed && (
+        <div>
+          <ul className="flex flex-col">
+            <Link href="/admin/edit">
+              <li className="p-4 flex text-lg border-b-2 border-white  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+                <FaRegEdit className="text-xl mr-2" /> Upload & Edit
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="flex p-4 text-lg border-b-2 border-white  hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+                <TiArrowBack className="text-2xl mr-1" /> Back to Website
+              </li>
+            </Link>
+            <li className=" flex p-4 text-lg border-b-2 border-white   hover:bg-[#4c4c4c] hover:transition-all hover:duration-500 ease-out duration-500">
+              <BiLogOutCircle className="text-2xl mr-1" /> Log Out
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
