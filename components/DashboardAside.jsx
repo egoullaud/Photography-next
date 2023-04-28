d38c58007bcd4a2bb9502eba48bff06c2a2409f9
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-28 15:56:30 -0700

    simplified

--- components/DashboardAside.jsx
+++ components/DashboardAside.jsx
@@ -5,28 +5,23 @@ function DashboardAside() {
   return (
     <div className="fixed top-0 bottom-0 left-0 bg-[#363636] text-white w-[20%] ">
       <div className="py-4">
-        <h1 className="text-4xl p-2">Welcome, Raine</h1>
+        <h1 className="text-4xl p-4">Welcome, Raine</h1>
       </div>
       <ul className="flex flex-col">
         <Link href="/admin/edit">
           {" "}
-          <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-            Upload Photos
-          </li>
-        </Link>
-        <Link href="/admin">
-          <li className="p-4 pl-2 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
-            Edit Galleries{" "}
+          <li className="p-4  text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+            Upload & Edit Photos
           </li>
         </Link>
 
         <Link href="/">
           {" "}
-          <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+          <li className="p-4 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
             Back to Website
           </li>
         </Link>
-        <li className="p-4 pl-10 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
+        <li className="p-4 text-lg border-b-2 border-white  hover:bg-white hover:text-[#363636] hover:transition-all hover:duration-500 ease-out duration-500">
           Log Out
         </li>
       </ul>
