e3e3208b2ebe59f1b6257a41d99661b3462ddbe6
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-28 14:47:06 -0700

    removed upload widget script

--- pages/admin/edit.jsx
+++ pages/admin/edit.jsx
@@ -8,6 +8,11 @@ export default function Edit() {
       <DashboardLayout>
         <UploadAssetForm />
       </DashboardLayout>
+      <script
+        defer
+        src="https://widget.cloudinary.com/v2.0/global/all.js"
+        type="text/javascript"
+      ></script>
     </div>
   );
 }
