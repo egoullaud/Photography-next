cd97a98c392bcd5e3116431cc57dbfa64a6e34b0
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-01 13:27:22 -0700

    emptied

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import DashboardLayout from "../../components/DashboardLayout";
+import Login from "../../components/Login";
 
 export default function Admin() {
   return <DashboardLayout></DashboardLayout>;
