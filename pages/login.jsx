8793217d05464bdfa66b3fb92969b5fadff25130
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-01 13:27:15 -0700

    login comp created

---
+++ pages/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../components/Login";
+import Layout from "../components/Layout";
+
+function login() {
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
+}
+
+export default login;
