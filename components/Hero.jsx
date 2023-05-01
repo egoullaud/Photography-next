a15196e692349583672ee9372c61912078c0646e
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-01 13:16:21 -0700

    created hygraph-linked hero comp

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import Image from "next/image";
 import heroImg from "../public/hero-img.jpg";
 
-function Hero() {
+function Hero({ heroes }) {
   return (
-    <div className="flex flex-col items-center justify-center  lg:mt-[5rem]">
-      {/* Hero area */}
-      <div
-        className="flex justify-center items-center text-center m-4
+    <>
+      {heroes.map((hero) => (
+        <div
+          key={hero.id}
+          className="flex flex-col items-center justify-center  lg:mt-[5rem]"
+        >
+          {/* Hero area */}
+          <div
+            className="flex justify-center items-center text-center m-4
                         md:text-xl md:mx-[2rem]
                         lg:text-3xl lg:mb-[5rem]"
-      >
-        <p className="italic tracking-wider">
-          “When words become unclear, I shall focus with photographs. When
-          images become inadequate, I shall be content with silence.”
-          <br />
-          <span className="not-italic">— Ansel Adams</span>
-        </p>
-      </div>
-      <Image priority src={heroImg} className="" alt="/" />
-    </div>
+          >
+            <p
+              dangerouslySetInnerHTML={{ __html: hero.heroQuote.html }}
+              className="tracking-wider"
+            ></p>
+          </div>
+          <Image
+            priority
+            src={hero.heroImage.url}
+            alt="#"
+            height={hero.heroImage.height}
+            width={hero.heroImage.width}
+          />
+        </div>
+      ))}
+    </>
   );
 }
 
