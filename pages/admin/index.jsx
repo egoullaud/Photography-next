ee846a6f7ffc266bb0a3b0eb4bd8dc5806ae4dc3
elizabeth.goullaud <egoullaud@gmail.com>
2023-05-01 18:32:15 -0700

    removed admin folder

--- pages/admin/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-import DashboardLayout from "../../components/DashboardLayout";
-import Login from "../../components/Login";
-
-export default function Admin() {
-  return <DashboardLayout></DashboardLayout>;
-}
